f7bd68de325c0d6b8362655e62a7c7ca8bef1d85
naval jadhav <pream.jadhav14@gmail.com>
2018-07-24 00:27:21 +0530

    Update lexical.c
    some update..

--- lexical.c
+++ lexical.c
@@ -3,7 +3,7 @@
 int main()
 {
 int i;
-char a[20];
+char a[20],b[20]; //anotner array
 printf("Enter the string");
 scanf("%s",a);
 int l=strlen(a);
@@ -14,10 +14,12 @@ if(a[i]>=65 && a[i]<=90|| a[i]>=97 && a[i]<=122)
    printf("%cis identifer",a[i]);
 printf("\n");
 }
-/*if(a[i]>=33 && a[i]<=47 || a[i]>=91 && a[i]<=96 || a[i]>=123 && a[i]<=126 ||a[i]>=48 && a[i]<=57) 
+while(a[i]>=33 && a[i]<=47 || a[i]>=91 && a[i]<=96 || a[i]>=123 && a[i]<=126 ||a[i]>=48 && a[i]<=57)  // update while
 {
-printf(strcat(a[i]),"it is operator\n");
-}*/
+   strcpy(b,a);         // here we're coping string from the loop and storing in 'array b' --strcpy(destination,source)
+   i++
+//printf(strcat(a[i]),"it is operator\n");
+}
 if(a[i]==42)
 {
 printf("* it multipliocation operator\n");
