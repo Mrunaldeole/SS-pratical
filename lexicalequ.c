315c7647c785d515f273c51b6721561f977d5f6d
amanraut46 <36956916+amanraut46@users.noreply.github.com>
2018-07-23 20:29:47 +0530

    Update lexicalequ.c

--- lexicalequ.c
+++ lexicalequ.c
@@ -1,42 +1,56 @@
 #include<stdio.h>
 #include<string.h>
-int main()
-{
-int i;
-char a[200],c;
-printf("Enter the string");
-scanf("%s",a);
-int l=strlen(a);
-for(i=0;i<l;i++)
-{
-if(a[i]>=65 && a[i]<=90|| a[i]>=97 && a[i]<=122)
-{
- c=a;
-printf("\n");
-}
-break;
-printf("identifier %c",c);
-/
-if(a[i]==42)
-{
-printf("* it multipliocation operator\n");
-}
-if(a[i]==43)
-{
-printf("+ it Addition operator\n");
-}
-if(a[i]==45)
-{
-printf("- it subtraction oprator\n");
-}
-if(a[i]==47)
-{
-printf("/ it multiplication operator\n");
-}
-if(a[i]==61)
-{
-printf("= it asignment operator\n");	
-}
-}
-}
+int i=0,len,current=0,prev=0;
 
+int check(char s[])
+{
+  if(65<=s[i] && s[i]<=90 || 97<=s[i] && s[i]<=122)
+  {
+    return(0);
+  }
+
+  if(32<=s[i] && s[i]<=46 || 58<=s[i] && s[i]<=64)
+  {
+    return(1);
+  }
+  if(48<=s[i] && s[i]<=57)
+  {
+    return(2);
+  }
+}
+void main()
+{ char string[100];
+  printf("Enter the string:");
+  scanf("%s",string);
+  prev=check(string);
+  current=check(string);
+
+  while(string[i] != '\0')
+  {
+    if(prev==current)
+    {
+      printf("%c",string[i]);
+    }
+    else
+    {
+      if(prev==0)
+      {
+        printf(" is an identifier\n");
+        printf("%c",string[i]);
+      }
+      if(prev==1)
+      {
+        printf(" is an arithmetic operator\n");
+        printf("%c",string[i]);
+      }
+      if(prev==2)
+      {
+        printf(" is a constant\n");
+        printf("%c",string[i]);
+      }
+    }
+    prev=current;
+    i++;
+    current=check(string);
+  }
+}
